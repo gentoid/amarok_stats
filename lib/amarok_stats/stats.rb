4451b04370e7541f88c2c45502ee1c6192877e1b
Viktor Lazarev <taurus101v@gmail.com>
2014-07-18 08:14:02 +0400

    It saves stats into DB (w/o tests)

--- lib/amarok_stats/stats.rb
+++ lib/amarok_stats/stats.rb
@@ -7,7 +7,14 @@ module AmarokStats
 
     def initialize
       @ratings = Hash.new(0)
+      @count = 0
       @query_result = []
+    end
+
+    def run
+      retrieve
+      calculate
+      save
     end
 
     def retrieve
@@ -19,9 +26,43 @@ module AmarokStats
     def calculate
       @query_result.each(as: :array) do |rating|
         @ratings[rating] += 1
+        @count += 1
+      end
+    end
+
+    def save
+      db = SQLite3::Database.new 'stats.db'
+
+      fields = []
+      values = []
+      @ratings.each do |rating, count|
+        fields += rating
+        values.push count
       end
 
-      puts @ratings
+      query = "INSERT INTO counts ('#{fields.join("', '")}', 'all') VALUES (#{values.join(', ')}, #{@count})"
+      puts query
+      db.execute query
+    end
+
+    def init_db
+      db = SQLite3::Database.new 'stats.db'
+
+      db.execute "CREATE TABLE counts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+         '0' INTEGER NOT NULL DEFAULT 0,
+         '1' INTEGER NOT NULL DEFAULT 0,
+         '2' INTEGER NOT NULL DEFAULT 0,
+         '3' INTEGER NOT NULL DEFAULT 0,
+         '4' INTEGER NOT NULL DEFAULT 0,
+         '5' INTEGER NOT NULL DEFAULT 0,
+         '6' INTEGER NOT NULL DEFAULT 0,
+         '7' INTEGER NOT NULL DEFAULT 0,
+         '8' INTEGER NOT NULL DEFAULT 0,
+         '9' INTEGER NOT NULL DEFAULT 0,
+        '10' INTEGER NOT NULL DEFAULT 0,
+        'all' INTEGER NOT NULL DEFAULT 0,
+        'datetime' TIMESTAMP DEFAULT CURRENT_TIMESTAMP)"
     end
 
   end
